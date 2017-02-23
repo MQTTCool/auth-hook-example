56f70fefc5c2f710737b90f91082f44c3ab4818f
Gianluca Finocchiaro <gianluca.finocchiaro@lightstreamer.com>
2017-02-23 10:47:53 +0100

    Rename to IPermissionInfo.

--- src/java/mqttextender/auth_demo/hooks/IPermissionInfo.java
+++ src/java/mqttextender/auth_demo/hooks/IPermissionInfo.java
@@ -13,38 +13,34 @@
 package mqttextender.auth_demo.hooks;
 
 /**
- * Simple interface which exposes queries to check authorized resources.
- *
+ * Simple interface which exposes queries to check authorized permissions.
  */
-public interface IAuthorizationInfo {
+public interface IPermissionInfo {
 
     /**
-     * Asks if connection to the specified MQTT broker address is allowed.
+     * Asks if permission to connect to the specified MQTT broker address is authorized.
      * 
      * @param broker
      *            the MQTT broker address to connect to
-     * @return {@code true} if connection to the specified MQTT broker address is allowed,
-     *         {@code false} otherwise
-     */
+      * @return {@code true} if permission is authorized, {@code false} otherwise
+F    */
     boolean allowConnectionTo(String broker);
 
     /**
-     * Asks if subscription to the specified topic is allowed.
+     * Asks if permission to subscribe to the specified topic is authorized.
      * 
      * @param topic
      *            the topic to subscribe to
-     * @return {@code true} if subscription to the specified topic is allowed, {@code false}
-     *         otherwise
+     * @return {@code true} if permission is authorized, {@code false} otherwise
      */
     boolean allowSubscribeTo(String topic);
 
     /**
-     * Asks if publishing to the specified topic is allowed.
+     * Asks if permission to publish to the specified topic is authorized.
      * 
      * @param topic
      *            the topic to which publishing a MQTT message.
-     * @return {@code true} if publishing to the specified topic is allowed, {@code false}
-     *         otherwise
+     * @return {@code true} if permission is authorized, {@code false} otherwise
      */
     boolean allowPublishTo(String topic);
 
